e4b5c3e954f8da2b5ab80f2da30f7672ecbef768
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-06-10 14:52:46 -0500

    Update GlobalStyleProvider.tsx

---
+++ taskapp/app/providers/GlobalStyleProvider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react"
+
+interface Props {
+    children: React.ReactNode;
+}
+
+function GlobalStyleProvider() {
+    return (
+        <div></div>
+    )
+}
+
+export default GlobalStyleProvider
