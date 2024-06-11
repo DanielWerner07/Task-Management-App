e42207d26e0d856619e2c52839db6b5a726b4bb2
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-06-11 16:52:40 -0500

    test theme

--- taskapp/app/Components/Sidebar/sidebar.tsx
+++ taskapp/app/Components/Sidebar/sidebar.tsx
@@ -1,12 +1,17 @@
 "use client";
+import { useGlobalState } from "@/app/context/globalProvider";
 import React from "react";
 import styled from "styled-components";
 
 function Sidebar() {
+const {theme} = useGlobalState();
+
+console.log(theme);
+
     return <SidebarStyled>Sidebar</SidebarStyled>
 }
 
 const SidebarStyled = styled.nav`
-    
+
 `;
 export default Sidebar;
