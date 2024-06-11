dd136618c60005ccb13cb5ce7c4a81ab649ebb23
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-06-11 16:32:14 -0500

    update

--- taskapp/app/Components/Sidebar/sidebar.tsx
+++ taskapp/app/Components/Sidebar/sidebar.tsx
@@ -1,8 +1,12 @@
 "use client";
 import React from "react";
+import styled from "styled-components";
 
 function Sidebar() {
-    return <div>Sidebar</div>
+    return <SidebarStyled>Sidebar</SidebarStyled>
 }
 
+const SidebarStyled = styled.nav`
+    
+`;
 export default Sidebar;
--- taskapp/app/providers/GlobalStyleProvider.tsx
+++ taskapp/app/providers/GlobalStyleProvider.tsx
@@ -14,7 +14,7 @@ const GlobalStyles = styled.div`
     padding: 2.5rem;
     display: flex;
     gap: 2.5rem;
-    height: 100%;
+    height: 
     `;
 
 export default GlobalStyleProvider
