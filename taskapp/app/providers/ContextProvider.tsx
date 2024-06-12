7e989f2c75f1e6e4f6e6c1fa0df32477f1a62319
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-06-12 14:40:52 -0500

    reset

--- taskapp/app/providers/ContextProvider.tsx
+++ taskapp/app/providers/ContextProvider.tsx
@@ -7,21 +7,6 @@ interface Props {
 }
 
 function ContextProvider({ children }: Props) {
-  const [isReady, setIsReady] = React.useState(false);
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      setIsReady(true);
-    }, 250);
-  }, []);
-
-  if (!isReady) {
-    return (
-      <div className="w-full h-full flex items-center justify-center">
-        <span className="loader"></span>
-      </div>
-    );
-  }
 
   return (
     <GlobalProvider>
