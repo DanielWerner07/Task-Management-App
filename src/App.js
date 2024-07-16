2e4e3a9f20e008b1f012049adbcfb4c83c7e9f39
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-07-16 16:27:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
-import Login from './Login';
+import AuthForm from './AuthForm';
 import Register from './Register';
 import CreateTask from './CreateTask';
 import withAuth from './withAuth';
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<AuthForm />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={withAuth(Home)} />
         <Route path="/create-task" element={withAuth(CreateTask)} />
