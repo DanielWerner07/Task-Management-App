0f0b263ec555af1de1b4e72c4f18ecf7e685a86e
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-07-16 14:40:44 -0500

    trying to add routing

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AuthForm from './AuthForm';
+import Home from './Home'; // Assuming you have a Home component
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <AuthForm />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/" component={AuthForm} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
