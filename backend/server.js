8c7de291bdad0034d1886889c1c33b8974cf688b
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-07-22 18:20:27 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const createTables = () => {
       id INT AUTO_INCREMENT PRIMARY KEY,
       username VARCHAR(255) NOT NULL,
       password VARCHAR(255) NOT NULL,
-      email VARCHAR(255) NOT NULL
+      email VARCHAR(255) NULL
     )
   `;
 
