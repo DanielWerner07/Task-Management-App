578d31eb265f138b24c4ade7d2144ae50d83abad
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-07-22 18:13:47 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,8 @@ const createTables = () => {
     CREATE TABLE IF NOT EXISTS users (
       id INT AUTO_INCREMENT PRIMARY KEY,
       username VARCHAR(255) NOT NULL,
-      password VARCHAR(255) NOT NULL
+      password VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL
     )
   `;
 
