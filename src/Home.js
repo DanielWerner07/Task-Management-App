e794115c5fdb5aff498f0114fbc4d5a7683e73c4
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2024-08-01 16:35:22 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { gapi } from 'gapi-script';
-import config from './config';
+import config from './config.js';
 import './Home.css';
 
 const Home = () => {
